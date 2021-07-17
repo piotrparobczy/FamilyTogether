c4643e16ee236b32f8a26b5e7258d0c3ab7065b3
piotrparobczy <666thenumberofhell>
2021-07-17 21:57:45 +0200

    init commit

---
+++ src/test/java/webapp/FamilyTogether/FamilyTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.FamilyTogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
