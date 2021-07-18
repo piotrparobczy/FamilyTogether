8d7f5840697e32745a3a188b7ea7a5e5fdf4b824
piotrparobczy <666thenumberofhell>
2021-07-18 18:04:42 +0200

    test of database

--- src/main/java/webapp/FamilyTogether/Model/Calendar.java
+++ src/main/java/webapp/FamilyTogether/Model/Calendar.java
@@ -1,26 +1,34 @@
 package webapp.FamilyTogether.Model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 @Table(name = "calendar")
 public class Calendar {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    //id form family
+    private Long id;
+    @ManyToOne
+    @JoinColumn
+    private Family family;
     private String event;
     private LocalDate date;
-    //user_id
+    @ManyToOne
+    @JoinColumn
+    private User user;
 }
--- src/main/java/webapp/FamilyTogether/Model/User.java
+++ src/main/java/webapp/FamilyTogether/Model/User.java
@@ -1,26 +1,42 @@
 package webapp.FamilyTogether.Model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
+    @Column(nullable = false)
     private LocalDate birthday;
-
+    @ManyToMany
+    @JoinTable(name = "user_family",
+            joinColumns = @JoinColumn(name = "idFamily"),
+            inverseJoinColumns = @JoinColumn(name = "idUser"))
+    private List<Family> idFamily = new ArrayList<>();
 }
